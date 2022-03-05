fb85aad2471a6e26cefa3774b5d0e53360b859d1
= <d.sek464@gmail.com>
2022-03-06 00:10:48 +0100

    changed welcome message

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,9 +6,8 @@ import styled from "styled-components";
 
 const home = (width)=>{
     return (
-        <div>
-            <h1 style={{fontSize:'5em'}} >Hi!</h1>
-            <h4> </h4>
+        <div style={{fontSize:'1.5em'}}>
+            <h1 style={{fontWeight:600}} >Welcome!</h1>
             {width > 800 ? <h4>Use the <AttentionSpan>ARROWS</AttentionSpan> on your <AttentionSpan>KEYBOARD</AttentionSpan> {<br />} 
             or the <AttentionSpan>CONTROLS</AttentionSpan> on the <AttentionSpan>SIDES </AttentionSpan>{<br />}
             to navigate through the site</h4> 
