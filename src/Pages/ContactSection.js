cb3fdf408785c87324757f1c40135027b615cc56
= <d.sek464@gmail.com>
2022-03-15 22:29:31 +0100

    contact section

--- src/Pages/ContactSection.js
+++ src/Pages/ContactSection.js
@@ -2,15 +2,19 @@ import Section from "Components/Section";
 const ContactSection = () => {
     return (
         <Section id="contact" title="get in touch">
-        <div className='left-side'>
-            form123
-        </div>
 
-        <div className='right-side'>
-            
-        </div>
+        <form name="contact" method="POST" data-netlify="true">
+        <h1>Get in touch</h1>
+            <input type="text" name="name" placeholder="Your name" required />
+            <input type="email" name="email" placeholder="Your email" required />
+            <textarea name="message" placeholder="Your message" required></textarea>
+            <input type="hidden" name="_template" value="table"/>
+
+                <button type="submit" >Send</button>
+        </form>
       </Section>
       );
 }
  
 export default ContactSection;
+
