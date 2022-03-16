4f3f1da8f7f42c5b40f37352be7fc3b2db6fbe81
= <d.sek464@gmail.com>
2022-03-16 19:29:17 +0100

    .

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { AiOutlineGithub, AiOutlineLink } from "react-icons/ai";
 export default function ProjectCard(params) {
     return (
