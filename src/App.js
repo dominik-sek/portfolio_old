1679da5f81118ee5778082897bcda1d100d00d26
Dominik Sęk <d.sek464@gmail.com>
2023-04-15 21:10:42 +0200

    fix redirects

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 function App() {
     const navigateToGithub = () =>{
-        window.location.href = 'github.com/gothic459';
+        window.location.href = 'https://github.com/gothic459';
     }
 
     React.useEffect(() => {
